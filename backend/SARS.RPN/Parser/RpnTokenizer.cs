0001c0d05e3d7b3eb571476f80af4a45a58123b4
Marcus Machaba <marcus@mvttech.co.za>
2025-06-11 14:35:05 +0200

    Add Tokenizer impl

--- backend/SARS.RPN/Parser/RpnTokenizer.cs
+++ backend/SARS.RPN/Parser/RpnTokenizer.cs
@@ -7,5 +7,18 @@ namespace SARS.RPN.Parser;
 /// </summary>
 public static class RpnTokenizer
 {
-    
+    private static readonly Regex _number = new(@"^\d+(\.\d+)?$", RegexOptions.Compiled);
+
+    public static IEnumerable<Token> Tokenize(string input)
+    {
+        foreach (var part in input.Split(' ', StringSplitOptions.RemoveEmptyEntries))
+        {
+            if (_number.IsMatch(part))
+                yield return new(TokenType.Number, part);
+            else if ("+-*/".Contains(part))
+                yield return new(TokenType.Operator, part);
+            else
+                throw new RpnParseException($"Invalid token '{part}'.");
+        }
+    }
 }
