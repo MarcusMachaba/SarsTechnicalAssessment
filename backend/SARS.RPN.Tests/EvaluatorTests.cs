4d9e7b6c793ba5d517fa3121869fc4b7fe441f5b
Marcus Machaba <marcus@mvttech.co.za>
2025-06-11 14:37:28 +0200

    Add UnitTests Evaluator impl

--- backend/SARS.RPN.Tests/EvaluatorTests.cs
+++ backend/SARS.RPN.Tests/EvaluatorTests.cs
@@ -4,5 +4,11 @@ namespace SARS.RPN.Tests;
 
 public class EvaluatorTests
 {
-    
+    [Fact]
+    public void Evaluates_Addition()
+        => Assert.Equal(7m, Evaluator.Evaluate("3 4 +"));
+
+    [Fact]
+    public void Throws_On_Invalid_Rpn()
+        => Assert.Throws<RpnParseException>(() => Evaluator.Evaluate("3 +"));
 }
