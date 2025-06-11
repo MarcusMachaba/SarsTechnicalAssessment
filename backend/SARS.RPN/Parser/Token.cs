8d9eddd47e445210d7e18728c4e5c9d60a2c0800
Marcus Machaba <marcus@mvttech.co.za>
2025-06-11 14:36:18 +0200

    Add Token model

--- backend/SARS.RPN/Parser/Token.cs
+++ backend/SARS.RPN/Parser/Token.cs
@@ -3,4 +3,6 @@
 /// <summary>
 /// Distinguishes numbers from operators while tokenising an RPN string.
 /// </summary>
+public enum TokenType { Number, Operator }
 
+public sealed record Token(TokenType Type, string Lexeme);
