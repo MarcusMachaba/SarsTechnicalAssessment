aaed7e7ba1875dbec427f6d74eb1ca00b78ae188
Marcus Machaba <marcus@mvttech.co.za>
2025-06-11 14:33:44 +0200

    Add InfixConverter impl

--- backend/SARS.RPN/Parser/InfixConverter.cs
+++ backend/SARS.RPN/Parser/InfixConverter.cs
@@ -5,5 +5,28 @@
 /// </summary>
 public static class InfixConverter
 {
-   
+    public static string ToInfix(string rpn)
+    {
+        var stack = new Stack<string>();
+
+        foreach (var tok in RpnTokenizer.Tokenize(rpn))
+        {
+            switch (tok.Type)
+            {
+                case TokenType.Number:
+                    stack.Push(tok.Lexeme);
+                    break;
+                case TokenType.Operator when stack.Count >= 2:
+                    var right = stack.Pop();
+                    var left = stack.Pop();
+                    stack.Push($"({left} {tok.Lexeme} {right})");
+                    break;
+                default:
+                    throw new RpnParseException("Malformed expression.");
+            }
+        }
+        return stack.Count == 1
+            ? stack.Pop()
+            : throw new RpnParseException("Unbalanced expression.");
+    }
 }
