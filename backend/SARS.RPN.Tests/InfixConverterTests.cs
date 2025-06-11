f6c6a24ad21ff93414f8af6490c7262378502dbd
Marcus Machaba <marcus@mvttech.co.za>
2025-06-11 14:38:27 +0200

    Add UnitTests InfixConveter impl

--- backend/SARS.RPN.Tests/InfixConverterTests.cs
+++ backend/SARS.RPN.Tests/InfixConverterTests.cs
@@ -4,5 +4,16 @@ namespace SARS.RPN.Tests;
 
 public class InfixConverterTests
 {
-   
+    [Theory]
+    [InlineData("3 4 +", "(3 + 4)")]
+    [InlineData("3 4 5 * +", "(3 + (4 * 5))")]
+    [InlineData("2.5 4.2 +", "(2.5 + 4.2)")]
+    public void Converts_Valid_Rpn(string rpn, string expected)
+        => Assert.Equal(expected, InfixConverter.ToInfix(rpn));
+
+    [Theory]
+    [InlineData("3 a +")]
+    [InlineData("3 +")]
+    public void Throws_On_Invalid_Rpn(string rpn)
+        => Assert.Throws<RpnParseException>(() => InfixConverter.ToInfix(rpn));
 }
